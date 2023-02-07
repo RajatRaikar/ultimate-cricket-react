94b82e650cb5edc553803db16e58bd716557ce70
Rajat R Raikar <rajat.r@juegostudioz.com>
2023-02-07 17:00:00 +0530

    register page change

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,124 +1,147 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 import Lottie from "lottie-react";
 import * as testJson from '../../lotties-data/home-screen-data.json';
 import { useNavigate } from 'react-router-dom'
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+import { Dna } from  'react-loader-spinner';
 
-// import './register.css';
+
 
 const Wrapper = styled.div`
-    width: 25%;
-  height: 500px;
-  position: fixed;
-  margin: auto;
-  top: 19%;
-  left: 38%;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  background-image: url(images/cricket-stadium.jpg);
+  /* filter: backdrop-blur(100px); */
+  background-size: cover;
+  flex-wrap: wrap;
+  /* background-repeat: no-repeat; */
+  background-position: center;
+  /* font-family: 'Itim', cursive; */
+`
+
+const RegisterDiv = styled.div`
+  /* background-color: rgba(2, 243, 247, 0.5); */
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 30%;
+  height: 50%;
 `
 
 const Input = styled.input`
-border-radius: 8px;
+  border-radius: 25px;
   border: 2px solid #dddfe2;
   outline: none;
   color: #1d2129;
   margin: 0.5rem 0;
   padding: 0.5rem 0.75rem;
-  width: 92%;
-  font-size: 1rem;
+  width:75%;
+  height: 52px;
+  font-size: 1.2rem;
+  /* font-family: 'Itim', cursive; */
 `
 const Button = styled.button`
-background: #1877f2;
-  border: 1px solid #1877f2;
-  color: #fff;
-  font-size: 1.25rem;
-  padding: 0.5rem;
-  margin: 0.5rem 0;
-  border-radius: 8px;
-  outline: none;
-  cursor: pointer;
-  text-align: center;
+  width: 150px;
+  height: 50px;
+  background-color: #4CAF50; /* Green background */
+  border: none; /* Remove border */
+  color: white; /* White text */
+  padding: 15px 32px; /* Add padding */
+  text-align: center; /* Center text */
+  text-decoration: none; /* Remove underline */
+  display: inline-block; /* Display inline */
+  font-size: 16px; /* Set font size */
+  margin: 4px 2px; /* Add margin */
+  border-radius: 50px; /* Round the corners */
 `
 const AnimationLogo = styled.div`
     width: 20rem;
     height: 20rem;
-    
+   
+   
 `
+
 const Register = () => {
-    const [ user, setUser] = useState({
-        name: "",
-        email:"",
-        password:"",
-        reEnterPassword: ""
+  const [ user, setUser] = useState({
+    name: "",
+    email:"",
+    password:"",
+    reEnterPassword: ""
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const userNavigateHook = useNavigate()
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  }
+
+  const register = () => {
+    setLoading(true);
+    Axios.post("https://ultimate-cricket.onrender.com/user/register", {
+      email: user.email,
+      name: user.name,
+      password: user.password,
+      repeat_password: user.reEnterPassword
+    }).then(res => {
+      setLoading(false);
+      let result = res.data;
+      console.log(result, 'register');
+      if(result.response_code === 201) {
+        result = res.data.response_data;
+        const errorString1 = result.errorObj.email_param !== undefined ? `${result.errorObj.email_param.error}\n` : '';
+        const errorString2 = result.errorObj.name_param !== undefined ? `${result.errorObj.name_param.error}\n` : '' ;
+        const errorString3 = result.errorObj.password_param !== undefined ? `${result.errorObj.password_param.error}\n` : '' ;
+
+        const errorString4 =  result.errorObj.repeat_password_param !== undefined ? `${result.errorObj.repeat_password_param.error}\n` : '';
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: errorString1+ errorString2+errorString3+ errorString4
+        })
+      } else if (result.response_code === 202) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'email already exist try to login'
+        }) 
+      } else if (result.response_code === 500) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'something went wrong'
+        }) 
+      } else {
+        localStorage.setItem('access_token', res.data.response_data.access_token);
+        userNavigateHook("/home");
+      }
     });
-
-    const userNavigateHook = useNavigate()
-
-    const handleChange = e => {
-        const { name, value } = e.target;
-        console.log(name, value);
-        setUser({ ...user, [name]: value });
-    }
-
-    const register = () => {
-            Axios.post("https://ultimate-cricket.onrender.com/user/register", {
-              email: user.email,
-              name: user.name,
-              password: user.password,
-              repeat_password: user.reEnterPassword
-            })
-            .then( res => {
-              let result = res.data;
-              console.log(result, 'register');
-              if(result.response_code === 201) {
-                result = res.data.response_data;
-                const errorString1 = result.errorObj.email_param !== undefined ? `${result.errorObj.email_param.error}\n` : '';
-                const errorString2 = result.errorObj.name_param !== undefined ? `${result.errorObj.name_param.error}\n` : '' ;
-                const errorString3 = result.errorObj.password_param !== undefined ? `${result.errorObj.password_param.error}\n` : '' ;
-
-                const errorString4 =  result.errorObj.repeat_password_param !== undefined ? `${result.errorObj.repeat_password_param.error}\n` : '';
-
-                Swal.fire({
-                  icon: 'error',
-                  title: 'Oops...',
-                  text: errorString1+ errorString2+errorString3+ errorString4
-                })
-              } else if (result.response_code === 202) {
-                Swal.fire({
-                  icon: 'error',
-                  title: 'Oops...',
-                  text: 'email already exist try to login'
-                }) 
-              } else if (result.response_code === 500) {
-                Swal.fire({
-                  icon: 'error',
-                  title: 'Oops...',
-                  text: 'something went wrong'
-                }) 
-              } else {
-                localStorage.setItem('access_token', res.data.response_data.access_token);
-                userNavigateHook("/home");
-              }
-            })
-        
-    }
-    return (
-        <>
-        <Wrapper>
-            <h1 style={{"text-align": "center"}}>REGISTER</h1>
-                    <Input type="text" name="name" value={user.name} placeholder="Your Name" onChange={ handleChange }></Input><br></br>
-                    {/* {Object.keys(error.name_param).length > 0 && Object.keys(error.name_param).includes("error") ?  error.name_param.error : ''} */}
-                    <Input type="text" name="email" value={user.email} placeholder="Your Email" onChange={ handleChange }></Input><br></br>
-                    {/* {error != null && Object.keys(error).length > 0 ? error.email_param.error : ''} */}
-                    <Input type="password" name="password" value={user.password} placeholder="Your Password" onChange={ handleChange }></Input><br></br>
-                    {/* {Object.keys(error).length > 0 ? Object.keys(error.password_param) : ''} */}
-                    <Input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Re-enter Password" onChange={ handleChange }></Input><br></br>
-                    {/* {Object.keys(error).length > 0 ? Object.keys(error.repeat_password_param) : ''} */}
-                    <Button onClick={register}> Register </Button> <Button onClick={() => { userNavigateHook("/login")}}> Login </Button>
-                  <AnimationLogo><Lottie animationData={testJson} loop={true}/></AnimationLogo>  
-        </Wrapper>
-        </>        
-    )
+  }
+  return (
+    <>
+    <Wrapper>
+      {
+        loading ?  <RegisterDiv> <Dna visible={true} height="80" color="#4fa94d" width="80" ariaLabel="dna-loading" wrapperStyle={{}} wrapperClass="dna-wrapper"/> </RegisterDiv>:
+        <RegisterDiv>
+          <h1>REGISTER</h1>
+          <Input type="text" name="name" value={user.name} placeholder="Your Name" onChange={ handleChange }></Input><br></br>
+          <Input type="text" name="email" value={user.email} placeholder="Your Email" onChange={ handleChange }></Input><br></br>
+          <Input type="password" name="password" value={user.password} placeholder="Your Password" onChange={ handleChange }></Input><br></br>
+          <Input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Re-enter Password" onChange={ handleChange }></Input><br></br>
+          <Button onClick={register}> Register </Button> <Button onClick={() => { userNavigateHook("/login")}}> Login </Button> 
+          {/* <AnimationLogo><Lottie animationData={testJson} loop={true}/></AnimationLogo>   */}
+        </RegisterDiv>
+      }     
+    </Wrapper>
+    </>        
+  )
 }
 
-export default Register
+export default Register;
--- src/components/userProfile/userProfile.js
+++ src/components/userProfile/userProfile.js
@@ -12,7 +12,7 @@ import { decodeToken } from "react-jwt";
 
 
 const Wrapper = styled.div`
-  width: 100wh;
+  width: 100vw;
   height: 100vh;
   display: grid;
   grid-template-columns: 12fr 12fr 12fr;
